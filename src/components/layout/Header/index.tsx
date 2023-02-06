e41133d051bec0648cd8fc30693af93fb8b13a18
Lucila Paiva <lucila.paiva@gmail.com>
2023-02-06 15:28:29 -0300

    Creación del Layout

---
+++ src/components/layout/Header/index.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+      <header>
+        <div className="container">
+          <div >
+            Header
+          </div>
+        </div>
+      </header>
+    );
+  };
+  
+  export { Header };
---
+++ src/components/layout/Main/index.tsx
@@ -0,0 +1,13 @@
+const Main = () => {
+    return (
+      <header>
+        <div className="container">
+          <div >
+            Main
+          </div>
+        </div>
+      </header>
+    );
+  };
+  
+  export { Main };
