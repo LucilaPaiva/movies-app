2180d5a846d608f213c184218f876523b16e69eb
Lucila Paiva <lucila.paiva@gmail.com>
2023-03-12 00:02:43 -0300

    popular movies se pintan. Falta dar estilos a la Grid/.

--- src/pages/Populars/index.tsx
+++ src/pages/Populars/index.tsx
@@ -1,13 +1,33 @@
-import { Layout } from "../../components"
+import { useEffect, useState } from "react";
+import { Layout } from "../../components";
+import { GridMovies } from "../../components/commons/GridMovies";
+import { getAllPopular } from "../../services/movies";
+
 
 const Populars  = () => {
 
+    const [movies, setMovies] = useState([]);
+    //const [popular, setPopular] = useState([]);
+
+useEffect(() => {
+
+        
+    getAllPopular().then(response => {
+        setMovies(response.results)})
+    
+}, []);
+
+
+
+
+
+
     return(
         <Layout>
-            Populares
+            <GridMovies items={movies} text={"Popular Movies"} />
         </Layout>
     )
     
     }
     
-    export { Populars } 
+    export { Populars }
