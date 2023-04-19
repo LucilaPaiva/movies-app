00374e07a7340b14e7515dbc2ddd0a5d4fa7cb9b
Lucila Paiva <lucila.paiva@gmail.com>
2023-04-18 23:20:45 -0300

    Creación de la carpeta hoc, hooks, Login, contexts. Carpeta services.

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,24 +1,67 @@
-import { SignUpForm } from "../types"
-import { api } from "../utils/axios"
+import { mapToArray } from "../helpers/mapToArray";
+import { SignUpPayload, User } from "../types";
+import { api } from "../utils/axios";
 
-const add = async(userPrueba: SignUpForm) => {
+const add = async (user: SignUpPayload) => {
+  const response = await api.post("/users.json", user);
+  return response.data;
+};
+
+const getAll = async (): Promise<User[]> => {
+  const response = await api.get("/users.json");
+  return mapToArray(response.data);
+};
+
+const getBy = async (type: "email" | "token", value: string) => {
+  const users = await getAll();
+
+  const user = users.find((user) => user[type] === value);
+
+  return user;
+};
+
+const update = ({ id, ...rest }: Partial<User>) => {
+  const response = api.patch(`/users/${id}.json`, { ...rest });
+};
+
+export const servicesUser = { add, getAll, getBy, update };
 
 
-    const response = await api.post ('/users.json', userPrueba)
-
-    return response.data
-
-}
-
-const getAll = async() => {
 
 
-    const response = await api.get ('/users.json')
+// import { SignUpForm, User } from "../types"
+// import { api } from "../utils/axios"
 
-    return response.data
-
-}
-
+// const add = async(userPrueba: SignUpForm) => {
 
 
-export const servicesUser = { add, getAll }
+//     const response = await api.post ('/users.json', userPrueba)
+
+//     return response.data
+
+// }
+
+// const getAll = async() => {
+
+
+//     const response = await api.get ('/users.json')
+
+//     return response.data
+
+// }
+
+// const getBy = async (type: "email" | "token", value: string) => {
+//     const users = await getAll();
+  
+//     const user = users.find((user) => user[type] === value);
+  
+//     return user;
+//   };
+  
+//   const update = ({ id, ...rest }: Partial<User>) => {
+//     const response = api.patch(`/users/${id}.json`, { ...rest });
+//   };
+
+
+
+// export const servicesUser = { add, getAll, getBy, update }
